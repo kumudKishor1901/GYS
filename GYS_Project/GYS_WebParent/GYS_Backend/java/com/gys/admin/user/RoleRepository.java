4c535f9af30defac858d881bfff66c993348184b
kumudKishor1901 <kumudkishor1901@gmail.com>
2023-08-02 21:45:08 +0530

    Coding Backend And Common module. Entity class Role in GYS_Common Role Repository in Backend Module spring hibernate Specification in application .properties file in backend module

---
+++ GYS_Project/GYS_WebParent/GYS_Backend/java/com/gys/admin/user/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.gys.admin.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gys.common.entity.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer>  {
+
+}
