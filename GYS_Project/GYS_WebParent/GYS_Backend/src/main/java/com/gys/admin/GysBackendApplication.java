fe9c7676dc33e78befd194b0a1d8896a0ef31abe
kumudKishor1901 <kumudkishor1901@gmail.com>
2023-08-02 20:04:46 +0530

    Admin Control Panel (GYS_Backend module build)

---
+++ GYS_Project/GYS_WebParent/GYS_Backend/src/main/java/com/gys/admin/GysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gys.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GysBackendApplication.class, args);
+	}
+
+}
