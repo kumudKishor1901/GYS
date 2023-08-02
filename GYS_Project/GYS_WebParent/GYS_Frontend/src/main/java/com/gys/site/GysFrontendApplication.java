2c83acc9e6f1934738c5a05b9f1bf0a8660ca413
kumudKishor1901 <kumudkishor1901@gmail.com>
2023-08-02 20:15:35 +0530

    GYS Frontend Module ready to code

---
+++ GYS_Project/GYS_WebParent/GYS_Frontend/src/main/java/com/gys/site/GysFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.gys.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GysFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GysFrontendApplication.class, args);
+	}
+
+}
